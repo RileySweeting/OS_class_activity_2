fe47abe88f3d0185cccaa272160d63df3ce7eccd
Riley Sweeting <rileyts2004@gmail.com>
2025-01-17 13:53:52 -0500

    Initial commit

---
+++ happy_new_year.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Variables
+    int count = 10;
+
+    // Count down from 10 to 0
+    while (count > 0) {
+        printf("%d \n", count);
+        count = count - 1;
+    }
+
+    // Display
+    printf("Happy New Year! \n");
+
+    // Return
+    return 0;
+}
