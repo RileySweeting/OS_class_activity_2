74213c5fdb3a94266a498a583712e2af120648c4
Riley Sweeting <rileyts2004@gmail.com>
2025-01-17 14:05:40 -0500

    Count to 100 multiples of 10

---
+++ count100.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Variables
+    int i;
+
+    // Display multiples of 10 up to 100
+    for (i = 10; i <=100; i = i + 10) {
+        printf("%d \n", i);
+    }
+
+    // Return
+    return 0;
+}
